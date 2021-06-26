a89281f74fb1fdc2161f579223ef5d3d1ce594d4
Anu Shibin <anushibin007@gmail.com>
2021-06-26 23:03:12 +0530

    Added Navbar and all basic npm packages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "animate.css/animate.min.css";
+import { Container } from "react-bootstrap";
+import Navigation from "./components/Navigation";
+
 function App() {
-	return <div></div>;
+	return (
+		<Container>
+			<Navigation />
+		</Container>
+	);
 }
 
 export default App;
