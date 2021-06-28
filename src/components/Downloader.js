c29af88498dcb5a47299f2ccf548429fbc5402ea
Anu Shibin <anushibin007@gmail.com>
2021-06-28 19:26:18 +0530

    Added progress bar and error alert Part 2

--- src/components/Downloader.js
+++ src/components/Downloader.js
@@ -13,38 +13,39 @@ const Downloader = () => {
 		setDownloadUrl(e.target.value);
 	};
 
-	const handleDownloadClicked = (e) => {
-		e.preventDefault();
-		loadProgressBar();
-		DownloadService.downloadFile(configs.serverUrl, downloadUrl)
-			.then(() => {
-				//if download was successful
-				setDownloadUrl("");
-			})
-			.catch((err) => {
-				//if download failed
-				alert("Sorry, an error occured: " + err);
-			});
+	const handleDownloadClicked = () => {
+		if (downloadUrl !== "") {
+			loadProgressBar();
+			DownloadService.downloadFile(configs.serverUrl, downloadUrl)
+				.then(() => {
+					//if download was successful
+					setDownloadUrl("");
+				})
+				.catch((err) => {
+					//if download failed
+					alert("Sorry, an error occured: " + err);
+				});
+		} else {
+			alert("Please enter a download URL");
+		}
 	};
 
 	return (
 		<React.Fragment>
 			<h5>Cache files to Middleman</h5>
-			<form>
-				<InputGroup>
-					<InputGroup.Prepend>
-						<InputGroup.Text>
-							<i className="bi bi-link-45deg"></i>
-						</InputGroup.Text>
-					</InputGroup.Prepend>
-					<FormControl placeholder="Download URL" value={downloadUrl} onChange={handleDownloadUrlChanged} aria-label="Download URL" required />
-					<InputGroup.Append>
-						<Button onClick={handleDownloadClicked} type="submit">
-							<i className="bi bi-download"></i>&nbsp;Cache to Middleman
-						</Button>
-					</InputGroup.Append>
-				</InputGroup>
-			</form>
+			<InputGroup>
+				<InputGroup.Prepend>
+					<InputGroup.Text>
+						<i className="bi bi-link-45deg"></i>
+					</InputGroup.Text>
+				</InputGroup.Prepend>
+				<FormControl placeholder="Download URL" value={downloadUrl} onChange={handleDownloadUrlChanged} aria-label="Download URL" />
+				<InputGroup.Append>
+					<Button onClick={handleDownloadClicked} type="submit">
+						<i className="bi bi-download"></i>&nbsp;Cache to Middleman
+					</Button>
+				</InputGroup.Append>
+			</InputGroup>
 		</React.Fragment>
 	);
 };
