679867bc89f3560f75fd79b34227c6ba2df6fd28
Anu Shibin <anushibin007@gmail.com>
2021-06-28 22:32:52 +0530

    Added custom alert headings

--- src/components/CustomToast.js
+++ src/components/CustomToast.js
@@ -5,11 +5,17 @@ import { ToastContext } from "../contexts/ToastContext";
 const CustomToast = () => {
 	const [toastConfig, setToastConfig] = useContext(ToastContext);
 
+	const getAlertHeading = () => {
+		if (toastConfig.type === "danger") return "Hey!";
+		if (toastConfig.type === "warning") return "Hmm...";
+		if (toastConfig.type === "success") return "Great!";
+	};
+
 	return (
 		<div style={{ position: "fixed", bottom: 10, right: 10 }}>
 			<Toast onClose={() => setToastConfig({ ...toastConfig, show: false })} show={toastConfig.show} delay={3000} autohide>
 				<Alert color="default" variant={toastConfig.type}>
-					<Alert.Heading>Hey!</Alert.Heading>
+					<Alert.Heading>{getAlertHeading()}</Alert.Heading>
 					<p>{toastConfig.message}</p>
 				</Alert>
 			</Toast>
