cf1886b4989e2af3e48b3f760e476d576489540a
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2021-06-29 10:32:32 +0530

    Added Dark Mode

--- src/App.js
+++ src/App.js
@@ -9,8 +9,21 @@ import ExistingDownloads from "./components/ExistingDownloads";
 import { ConfigProvider } from "./contexts/ConfigContext";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Darkmode from "darkmode-js";
 
 function App() {
+	/**
+	 * Dark Mode Stuff
+	 */
+	const darkModeoptions = {
+		label: "🌓", // default: ''
+		autoMatchOsTheme: false, // default was true. But it had some glitches. So I disabled it
+	};
+	new Darkmode(darkModeoptions).showWidget();
+
+	/**
+	 * Rest of the App
+	 */
 	return (
 		<Container>
 			<ConfigProvider>
