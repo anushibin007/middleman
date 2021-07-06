13d02ab02fc946718872f9e8122dd3d03c40e239
Anu Shibin <anushibin007@gmail.com>
2021-07-06 21:21:17 +0530

    Allow requests from a specific set of hostnames only

---
+++ functions/constants.js
@@ -0,0 +1,3 @@
+module.exports = Object.freeze({
+	allowedServers: ["localhost", "us-central1-middleman-e3e91.cloudfunctions.net", "anushibin007.github.io"],
+});
--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,14 @@
 const functions = require("firebase-functions");
+const constants = require("./constants");
 
 const cors = require("cors")({ origin: true });
 
 exports.upload = functions.https.onRequest((request, response) => {
 	cors(request, response, () => {
-		functions.logger.info("Hello logs!", { structuredData: true });
-		response.send("Upload Triggered!");
+		if (constants.allowedServers.includes(request.hostname)) {
+			functions.logger.info("Hello logs!", { structuredData: true });
+			console.log(request.hostname);
+			response.send("Upload Triggered! by " + request.hostname);
+		}
 	});
 });
