8b867e9de692f05a010c6b79443e5c232c08c6ae
Anu Shibin <anushibin007@gmail.com>
2021-07-06 21:25:58 +0530

    Added a few logs

--- functions/index.js
+++ functions/index.js
@@ -6,10 +6,10 @@ const cors = require("cors")({ origin: true });
 exports.upload = functions.https.onRequest((request, response) => {
 	cors(request, response, () => {
 		if (constants.allowedServers.includes(request.hostname)) {
-			functions.logger.info("Hello logs!", { structuredData: true });
-			console.log(request.hostname);
+			functions.logger.info("Executing upload function", { "request.hostname": request.hostname });
 			response.send("Upload Triggered! by " + request.hostname);
 		} else {
+			functions.logger.error("Access Forbidden", { "request.hostname": request.hostname });
 			response.status(403).json({ error: "Access Forbidden" });
 		}
 	});
