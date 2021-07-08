b7074e2a23563e4911b9908977a60e380ef2ea8f
Anu Shibin <anushibin007@gmail.com>
2021-07-08 13:48:17 +0530

    Firebase functions should always return a Promise

--- functions/index.js
+++ functions/index.js
@@ -8,14 +8,16 @@ const db = admin.database();
 const uploadFunction = require("./handlers/upload");
 const getExistingFilesFunction = require("./handlers/getexisting");
 
-exports.upload = functions.https.onRequest((request, response) => {
-	cors(request, response, () => {
+exports.upload = functions.https.onRequest(async (request, response) => {
+	cors(request, response, async () => {
 		uploadFunction.handler(request, response, db, admin);
+		return true;
 	});
 });
 
-exports.getexisting = functions.https.onRequest((request, response) => {
-	cors(request, response, () => {
+exports.getexisting = functions.https.onRequest(async (request, response) => {
+	cors(request, response, async () => {
 		getExistingFilesFunction.handler(request, response, db);
+		return true;
 	});
 });
