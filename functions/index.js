4dcf4ec7320ca89722cd7b92a61b66e0760500db
Anu Shibin <anushibin007@gmail.com>
2021-07-09 18:43:23 +0530

    Refactored db to adminDb

--- functions/index.js
+++ functions/index.js
@@ -3,21 +3,21 @@ const admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
 
 const cors = require("cors")({ origin: true });
-const db = admin.database();
+const adminDb = admin.database();
 
 const uploadFunction = require("./handlers/upload");
 const getExistingFilesFunction = require("./handlers/getexisting");
 
 exports.upload = functions.https.onRequest(async (request, response) => {
 	cors(request, response, async () => {
-		uploadFunction.handler(request, response, db, admin);
+		uploadFunction.handler(request, response, adminDb, admin);
 		return true;
 	});
 });
 
 exports.getexisting = functions.https.onRequest(async (request, response) => {
 	cors(request, response, async () => {
-		getExistingFilesFunction.handler(request, response, db);
+		getExistingFilesFunction.handler(request, response, adminDb);
 		return true;
 	});
 });
