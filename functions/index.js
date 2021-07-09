fca936816a91af3ff4a25265dc5d969bf4de4c98
Anu Shibin <anushibin007@gmail.com>
2021-07-09 21:41:27 +0530

    Added a function to test Speed

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ const admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
 
 const cors = require("cors")({ origin: true });
+const speedTest = require("speedtest-net");
 const adminDb = admin.database();
 
 const uploadFunction = require("./handlers/upload");
@@ -19,5 +20,18 @@ exports.getexisting = functions.https.onRequest(async (request, response) => {
 	cors(request, response, async () => {
 		getExistingFilesFunction.handler(request, response, adminDb);
 		return true;
+	});
+});
+
+exports.getSpeed = functions.https.onRequest(async (request, response) => {
+	cors(request, response, async () => {
+		try {
+			speedTest({ acceptLicense: true }).then((speed) => {
+				console.log({ speed });
+			});
+			response.send("Check the server logs to see the speed");
+		} catch (err) {
+			response.status(500).send({ err });
+		}
 	});
 });
