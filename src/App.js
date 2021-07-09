310566ae34db5fa83f4b6d8fa11c80cc46adcde1
Anu Shibin <anushibin007@gmail.com>
2021-07-09 23:27:36 +0530

    Remove bootstrap-icons.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
 import "animate.css/animate.min.css";
 import { Container } from "react-bootstrap";
 import Navigation from "./components/Navigation";
